694f11ae3d1a35e9a944324d53c73452fc7237d2
daniel genario <dgenario@proveedor.teco.com.ar>
2025-07-17 16:20:22 -0300

    entregable coderHouse tabla de alumnos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AlumnoListaComponent } from './components/alumno-lista/alumno-lista.component';
+import { AlumnoFormComponent } from './components/alumno-form/alumno-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'alumnos', component: AlumnoListaComponent },
+  { path: 'alumnos/nuevo', component: AlumnoFormComponent },
+  { path: 'alumnos/editar/:id', component: AlumnoFormComponent },
+  { path: '', redirectTo: '/alumnos', pathMatch: 'full' }
+];
